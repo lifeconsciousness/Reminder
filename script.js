11cdb62a3ced7e845130151936274e46664a72cd
lifeconsciousness <lifeconsciousness321@gmail.com>
2024-05-10 13:05:36 +0200

    added random pos to the button

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ let minute = 60
 let song = new Audio('./king-gizz-countdown.mp3')
 
 window.onload = function () {
-  // display = document.querySelector('#time')
-  //   startTimer(5, display)
   startTimer(minute * 7)
 }
 
@@ -12,7 +10,31 @@ muteButton.addEventListener('click', () => {
   song.load()
 })
 
+muteButtonContainer = document.getElementById('btn-container')
+
+// with a chance moves button to some random position
+muteButton.addEventListener('mouseover', () => {
+  let chance = getRandomInt(1, 10)
+  if (chance < 4) {
+    randomPosButton()
+  }
+})
+
+function randomPosButton() {
+  let randTop = getRandomInt(10, window.innerHeight - 60)
+  let randLeft = getRandomInt(10, window.innerWidth - 200)
+
+  if (randTop < window.innerHeight / 2 - 25 && randTop > window.innerHeight / 2 - 100) {
+    randTop -= 80
+  }
+
+  muteButtonContainer.style.top = randTop + 'px'
+  muteButtonContainer.style.left = randLeft + 'px'
+}
+
 function startTimer(duration) {
+  randomPosButton()
+
   let timer = duration,
     minutes,
     seconds
@@ -24,16 +46,18 @@ function startTimer(duration) {
     seconds = seconds < 10 ? '0' + seconds : seconds
 
     // display.textContent = minutes + ':' + seconds
+    // console.log(minutes + ':' + seconds)
 
     if (--timer < 0) {
       console.log('time ran out')
       clearInterval(interval)
       song.play()
-      startTimer(getRandomInt(minute * 13, minute * 36))
+      startTimer(getRandomInt(minute * 13, minute * 32))
     }
   }, 1000)
 }
 
+// returns random int min max included
 function getRandomInt(min, max) {
   min = Math.ceil(min)
   max = Math.floor(max)
@@ -45,7 +69,7 @@ function getRandomInt(min, max) {
 mainText = document.getElementById('question')
 // mainText.innerHTML = ''
 
-const text = new Blotter.Text('‎‎Are you doing what you need to? Honestly‎‎', {
+const text = new Blotter.Text('‎‎‎‎Are you doing what you need to? Honestly‎‎‎‎', {
   family: 'Open Sans, sans-serif',
   // size: 48,
   size: 38,
@@ -57,7 +81,7 @@ const text = new Blotter.Text('‎‎Are you doing what you need to? Honestly‎
 
 let material = new Blotter.LiquidDistortMaterial()
 material.uniforms.uSpeed.value = 0.07
-material.uniforms.uVolatility.value = 0.015
+material.uniforms.uVolatility.value = 0.011
 material.uniforms.uSeed.value = 0.3
 
 let blotter = new Blotter(material, {
